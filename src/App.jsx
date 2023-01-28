c51c2cd7049966aa7e82f0b38d2c9415fcd51a42
Rubcs <rubcastellon@gmail.com>
2023-01-28 16:12:34 +0100

    add content to proyecto_4

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Figure from "./components/Figure";
 
 const NASA_URL = "https://api.nasa.gov/";
 const NASA_API_KEY = "nlEtySupmu0WGD2PwyBpxYXcrngbiSNhfmaFjVs2";
+const NASA_MARS_URL =
+  "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000";
 
 function App() {
   const today = new Date(Date.now()).toISOString().slice(0, 10);
@@ -12,6 +14,8 @@ function App() {
   const [date, setDate] = useState("");
 
   const [apod, setApod] = useState("");
+
+  const [marsData, setMarsData] = useState("");
 
   const handleInput = (ev) => {
     setDate(ev.target.value.toLocaleString());
@@ -30,14 +34,31 @@ function App() {
 
   console.log(apod); //Este console.log es para ver la data que recibimos.
 
+  useEffect(() => {
+    const getMarsData = async () => {
+      const response = await fetch(`${NASA_MARS_URL}&api_key=${NASA_API_KEY}`);
+      const data = await response.json();
+      setMarsData(data);
+    };
+    getMarsData();
+  }, [date]);
+
+  console.log(marsData);
+
   return (
     <div>
       <img src="../public/favicon-192.png"></img>
       <h1>Astronomic picture of the day</h1>
       <span>This picture is from the date: {date}</span>
       <br />
-      <input type="date" onChange={handleInput} />
-      <Figure data={apod} />
+      <input type="date" min="1995-06-16" max={today} onChange={handleInput} />
+      {/* {date > today ? (
+        <p>
+          Not picture generated fof that date yet. Please, choose a date between
+          Jun 16, 1995 and today
+        </p>
+      ) : ( */}
+      <Figure data={apod} martianData={marsData} />
     </div>
   );
 }
--- src/components/Figure.jsx
+++ src/components/Figure.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
-const Figure = ({ data }) => {
+export const DataBox = styled.div`
+  color: grey;
+  font-size: 12px;
+  font-style: italic;
+  text-align: left;
+  display: flex;
+  justify-content: space-between;
+  border-color: grey;
+  border-style: double;
+  border-radius: 24px;
+  width: 30%;
+`;
+
+export const CopyDate = styled.span`
+  margin: 10px;
+`;
+
+const Figure = ({ data, martianData }) => {
   return (
     <figure>
+      <h2>{data.title}</h2>
       <img src={data.url} alt={data.title} />
+      <DataBox>
+        <CopyDate>Copyright: {data.copyright}</CopyDate>
+        <CopyDate>{data.date}</CopyDate>
+      </DataBox>
+      <br />
+      <span>{data.explanation}</span>
     </figure>
   );
 };
