bce3ee4f65c42ab90c406ef4eaef140213103e9b
Rubcs <rubcastellon@gmail.com>
2023-01-31 12:49:01 +0100

    add content to proyecto_4_App_Nasa

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Figure from "./components/Figure";
 const NASA_URL = "https://api.nasa.gov/";
 const NASA_API_KEY = "nlEtySupmu0WGD2PwyBpxYXcrngbiSNhfmaFjVs2";
 const NASA_MARS_URL =
-  "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000";
+  "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?";
 
 function App() {
-  const today = new Date(Date.now()).toISOString().slice(0, 10); // const [time, setTime] = useState(today);
+  const today = new Date(Date.now()).toISOString().slice(0, 10);
+
+  const [marsDate, setMarsDate] = useState("");
 
   const [date, setDate] = useState("");
 
@@ -16,8 +18,14 @@ function App() {
 
   const [marsData, setMarsData] = useState([]);
 
+  const [selectedApi, setSelectedApi] = useState("APOD-API");
+
   const handleInput = (ev) => {
     setDate(ev.target.value.toLocaleString());
+  };
+
+  const handleMarsInput = (ev) => {
+    setMarsDate(ev.target.value.toLocaleString());
   };
 
   useEffect(() => {
@@ -36,7 +44,9 @@ function App() {
 
   useEffect(() => {
     const getMarsData = async () => {
-      const response = await fetch(`${NASA_MARS_URL}&api_key=${NASA_API_KEY}`);
+      const response = await fetch(
+        `${NASA_MARS_URL}earth_date=2015-5-3&api_key=${NASA_API_KEY}`
+      );
       const mData = await response.json();
       setMarsData(mData);
     };
@@ -45,30 +55,59 @@ function App() {
 
   console.log(marsData); //Este console.log es para ver la data que recibimos.
 
-  // const marsImage = marsData.photos.map((photo) => photo.img_src);
-
-  // console.log(marsImage[0]);
-
   return (
     <div>
       <img src="../public/favicon-192.png"></img>
       <h1>Astronomic picture of the day</h1>
       <span>This picture is from the date: {date}</span>
       <br />
+      <span>APOD API</span>
+      <br />
       <input type="date" min="1995-06-16" max={today} onChange={handleInput} />
+      <br />
+      <span>MARS API</span>
+      <br />
+      <input
+        type="date"
+        min="1995-06-16"
+        max={today}
+        onChange={handleMarsInput}
+      />
+
       {/* {date > today ? (
         <p>
-          Not picture generated fof that date yet. Please, choose a date between
+          Not picture generated for that date yet. Please, choose a date between
           Jun 16, 1995 and today
         </p>
       ) : ( * */}
-      <Figure data={apod} />
-      <select></select>
-      <div>
-        {marsData.photos.map((photo) => (
-          <img src={photo.img_src} key={photo.id} alt="Mars Photo" />
-        ))}
-      </div>
+      <br />
+      <p>PLEASE, SELECT YOUR DESIRED API:</p>
+      <select
+        name="select-API"
+        onChange={(e) => setSelectedApi(e.target.value)}
+      >
+        <option value="APOD-API" selected={selectedApi === "APOD-API"}>
+          APOD API
+        </option>
+        <option value="MARS-API" selected={selectedApi === "MARS-API"}>
+          MARS API
+        </option>
+      </select>
+
+      {selectedApi === "APOD-API" ? (
+        <Figure data={apod} />
+      ) : (
+        <div>
+          <h3>MARS' PICTURES FROM THE EARTH DATE SELECTED:</h3>
+          {marsData.photos ? (
+            marsData.photos.map((photo) => (
+              <img src={photo.img_src} key={photo.id} alt="Mars Photo" />
+            ))
+          ) : (
+            <p>-- (No pictures from Mars were taken on that date) --</p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
